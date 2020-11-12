acf6456b2830fe17c9cc47e28086287c88620774
rohitjain25 <www.rohitjain.com@gmail.com>
2020-11-13 03:26:42 +0530

    initial commit

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,41 @@
+ import React, {useState, useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState';
+
+export const AddTransaction = () => {
+  const [text, setText] = useState('');
+  const [amount, setAmount] = useState(0);
+
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text,
+      amount: +amount
+    }
+
+    addTransaction(newTransaction);
+  }
+
+  return (
+    <>
+      <h3>Add new transaction</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-control">
+          <label htmlFor="text">Text</label>
+          <input type="Text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Transaction Detail" />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount"
+            >Amount <br />
+            (Use '-' before amount for expense)</label
+          >
+          <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Amount" />
+        </div>
+        <button className="btn">Add transaction</button>
+      </form>
+    </>
+  )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
+
+export const Header = () =>{
+	return (
+
+		<h2>
+			<MonetizationOnIcon />
+			 Expenso : Track Your Expenses
+		</h2>
+
+		)
+}
